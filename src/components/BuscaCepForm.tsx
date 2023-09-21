c86b605bc1d9465193a679786fe7c61df60e3ec1
nanaflorencci <giovannaflorencci1814@gmail.com>
2023-09-21 15:16:14 -0300

    .

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,6 +1,8 @@
 import React, {useState, ChangeEvent, FormEvent, useEffect} from "react";
 
 import styles from "../App.module.css"
+import Header from "./Header";
+import Footer from "./Footer";
 
 const BuscaCep = () => {
 
@@ -33,6 +35,8 @@ const BuscaCep = () => {
 
     return (
         <div>
+            <Header/>
+            <main className={styles.main}>
             <form onSubmit={findCep}>
                 <label htmlFor="cep">CEP</label>
                 <input type="text" name="cep" id="cep"onChange={submitForm} />
@@ -42,6 +46,8 @@ const BuscaCep = () => {
             <p>Estado: {uf}</p> 
             <p>CEP: {cep}</p> 
             <p className={styles.error}>{erro}</p>
+            </main>
+            <Footer/>
         </div>
     );
 }
