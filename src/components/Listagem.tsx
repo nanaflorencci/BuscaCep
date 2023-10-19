2da5c39a06820b534ee025c228183f3f40462320
nanaflorencci <giovannaflorencci1814@gmail.com>
2023-10-19 10:20:35 -0300

    .

--- src/components/Listagem.tsx
+++ src/components/Listagem.tsx
@@ -1,63 +1,109 @@
-import React, {Component, useState, ChangeEvent, FormEvent, useEffect} from 'react';
+import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import styles from "../App.module.css";
 import { CadastroInterface } from '../interfaces/CadastroInterface';
 import axios from 'axios';
+import { text } from 'stream/consumers';
 
 const Listagem = () => {
-const [usuarios, setUsuarios] = useState<CadastroInterface[]>([]);
-const [error, setError] = useState("");
+    const [usuarios, setUsuarios] = useState<CadastroInterface[]>([]);
+    const [pesquisa, setPesquisa] = useState<string>('');
+    const [error, setError] = useState("");
 
-useEffect(() => {
-    async function fetchData(){
-        try{
-            const response = await axios.get('http://10.137.9.131:8000/api/find');
-            setUsuarios(response.data.data);
-
-        }catch(error){
-            setError ("Ocorreu um erro");
-            console.log (error);
+    const handleState = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "pesquisa") {
+            setPesquisa(e.target.value);
         }
-    } 
-    fetchData();
-}, []);
+    }
+
+    const buscar = (e: FormEvent) => {
+        e.preventDefault();
+        async function fetchData() {
+            try {
+                const response = await axios.post('http://10.137.9.134:8000/api/findNome',
+                    { nome: pesquisa },
+                    {
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function (response) {
+                        setUsuarios(response.data.data);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://10.137.9.134:8000/api/find');
+                setUsuarios(response.data.data);
+
+            } catch (error) {
+                setError("Ocorreu um erro");
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
 
     return (
-    <div>
-    <main className={styles.main}>
-        <div className='container'>
-            <div className='card'>
-                <div className='card-body'>
-                    <h5 className='card-title'>Listagem de Usuários</h5>
-                    <table className='table table-hover'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>CPF</th>
-                                <th>E-mail</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {usuarios.map(usuario => (
-                                <tr key={usuario.id}>
-                                <td>{usuario.id}</td>
-                                <td>{usuario.nome}</td>
-                                <td>{usuario.cpf}</td>
-                                <td>{usuario.email}</td>
-                                <td>
-                                    <a href="#" className='btn btn-dark btn-sm'>Editar</a>
-                                    <a href="#" className='btn btn-secondary btn-sm'>Excluir</a>
-                                </td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
+        <div>
+            <main className={styles.main}>
+                <div className='container'>
+                    <div className='col-md mb-3'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Pesquisar</h5>
+                                <form onSubmit={buscar} className='row'>
+                                    <div className='col-10'>
+                                        <input name="pesquisa" type="text" className='form-control' onChange={handleState} />
+                                    </div>
+                                    <div className='col-1'>
+                                        <button type='submit' className='btn btn-dark'>Pesquisar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>Listagem de Usuários</h5>
+                            <table className='table table-hover'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>CPF</th>
+                                        <th>E-mail</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {usuarios.map(usuario => (
+                                        <tr key={usuario.id}>
+                                            <td>{usuario.id}</td>
+                                            <td>{usuario.nome}</td>
+                                            <td>{usuario.cpf}</td>
+                                            <td>{usuario.email}</td>
+                                            <td>
+                                                <a href="#" className='btn btn-dark btn-sm'>Editar</a>
+                                                <a href="#" className='btn btn-secondary btn-sm'>Excluir</a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </main>
         </div>
-    </main>
-    </div>
     );
 }
 
